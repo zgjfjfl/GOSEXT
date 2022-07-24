79a7f578b2d506bbfd88cc422e7553c70a803cb7
zgjfjfl <243244615@qq.com>
2022-07-24 19:46:22 +0800

    Add files via upload

---
+++ Zgjfjfl_Ornn.lua
@@ -0,0 +1,329 @@
+
+local Heroes ={"Ornn"}
+
+require "GGPrediction"
+require "MapPositionGOS"
+
+local GameTurret = Game.Turret
+local GameTurretCount = Game.TurretCount
+local GameHeroCount     = Game.HeroCount
+local GameHero          = Game.Hero
+local GameMinionCount     = Game.MinionCount
+local GameMinion          = Game.Minion
+local GameJuCount     = Game.MinionCount
+local GameMinion          = Game.Minion
+
+local orbwalker         = _G.SDK.Orbwalker
+local HealthPrediction         = _G.SDK.HealthPrediction
+
+
+local function isSpellReady(spell)
+    return  myHero:GetSpellData(spell).currentCd == 0 and myHero:GetSpellData(spell).level > 0 and Game.CanUseSpell(spell) == 0
+end
+
+local function getDistanceSqr(Pos1, Pos2)
+    local Pos2 = Pos2 or myHero.pos
+    local dx = Pos1.x - Pos2.x
+    local dz = (Pos1.z or Pos1.y) - (Pos2.z or Pos2.y)
+    return dx^2 + dz^2
+end
+
+local function getDistance(Pos1, Pos2)
+    return math.sqrt(getDistanceSqr(Pos1, Pos2))
+end
+
+local function getEnemyHeroes()
+    local EnemyHeroes = {}
+    for i = 1, Game.HeroCount() do
+        local Hero = Game.Hero(i)
+        if Hero.isEnemy and not Hero.dead then
+            table.insert(EnemyHeroes, Hero)
+        end
+    end
+    return EnemyHeroes
+end
+
+local function isValid(unit)
+    if (unit and unit.valid and unit.isTargetable and unit.alive and unit.visible and unit.networkID and unit.pathing and unit.health > 0) then
+        return true;
+    end
+    return false;
+end
+
+local function getEnemyHeroesWithinDistanceOfUnit(location, distance)
+    local EnemyHeroes = {}
+    for i = 1, Game.HeroCount() do
+        local Hero = Game.Hero(i)
+        if Hero.isEnemy and not Hero.dead and Hero.pos:DistanceTo(location) < distance then
+            table.insert(EnemyHeroes, Hero)
+        end
+    end
+    return EnemyHeroes
+end
+
+local function getEnemyMinionsWithinDistanceOfLocation(location, distance)
+    local EnemyMinions = {}
+    for i = 1, Game.MinionCount() do
+        local minion = Game.Minion(i)
+        if minion and minion.isEnemy and not minion.dead and minion.pos:DistanceTo(location) < distance then
+            table.insert(EnemyMinions, minion)
+        end
+    end
+    return EnemyMinions
+end
+
+local function getAOEMinion(maxRange, abilityRadius) 
+    local bestCount = 0
+    local bestPosition = nil
+    for i = 1, GameMinionCount() do
+        local minion = GameMinion(i)
+        local count = 0
+        if minion and not minion.dead and minion.isEnemy and myHero.pos:DistanceTo(minion.pos) < maxRange then 
+            local count = #getEnemyMinionsWithinDistanceOfLocation(minion.pos, abilityRadius)
+            if count > bestCount then
+                bestCount = count
+                bestPosition = minion.pos
+            end
+        end
+    end
+    return bestPosition, bestCount
+end
+
+local function getEnemyHeroesWithinDistance(distance)
+    return getEnemyHeroesWithinDistanceOfUnit(myHero.pos, distance)
+end
+local function doesMyChampionHaveBuff(buffName)
+    for i = 0, myHero.buffCount do
+        local buff = myHero:GetBuff(i)
+        if buff.name == buffName and buff.count > 0 then 
+            return true
+        end
+    end
+    return false
+end
+
+local function getChampionBuffCount(buffName)
+    for i = 0, myHero.buffCount do
+        local buff = myHero:GetBuff(i)
+        if buff.name == buffName then 
+            return buff.count
+        end
+    end
+    return -1
+end
+
+local function printChampionBuffs(champ)
+    for i = 0, champ.buffCount do
+        local buff = champ:GetBuff(i)
+        if buff.count > 0 then 
+            print(string.format("%s - stacks: %f count %f",buff.name, buff.stacks, buff.count))
+        end
+    end
+end
+
+local function doesThisChampionHaveBuff(target, buffName)
+    for i = 0, target.buffCount do
+        local buff = target:GetBuff(i)
+        if buff.name == buffName and buff.count > 0 then 
+            return true
+        end
+    end
+    return false
+end
+
+local function isTargetImmobile(target)
+    local buffTypeList = {5, 8, 12, 22, 23, 25, 30, 35} 
+	for i = 0, target.buffCount do
+        local buff = target:GetBuff(i)
+        for _, buffType in pairs(buffTypeList) do
+		    if buff.type == buffType and buff.count > 0 then
+                return true, buff.duration
+            end
+		end
+	end
+	return false, 0
+end
+
+local function castSpell(spellData, hotkey, target)
+    local pred = GGPrediction:SpellPrediction(spellData)
+    pred:GetPrediction(target, myHero)
+    if pred:CanHit(GGPrediction.HITCHANCE_NORMAL) then
+        if myHero.pos:DistanceTo(pred.CastPosition) <= spellData.Range + 15 then
+            Control.CastSpell(hotkey, pred.CastPosition)	
+        end
+    end
+end
+
+local function castSpellHigh(spellData, hotkey, target)
+    local pred = GGPrediction:SpellPrediction(spellData)
+    pred:GetPrediction(target, myHero)
+    if pred:CanHit(GGPrediction.HITCHANCE_HIGH) then
+        Control.CastSpell(hotkey, pred.CastPosition)	
+    end
+end
+
+local _nextVectorCast = Game.Timer()
+local _nextSpellCast = Game.Timer()
+local _vectorMousePos = mousePos
+function VectorCast(startPos, endPos, hotkey)
+	if _nextSpellCast > Game.Timer() then return end	
+	if _nextVectorCast > Game.Timer() then return end
+
+	_nextVectorCast = Game.Timer() + 2
+	_nextSpellCast = Game.Timer() + .25
+    _vectorMousePos = mousePos
+
+	Control.SetCursorPos(startPos)	
+    orbwalker:SetMovement(false)
+    orbwalker:SetAttack(false)
+    
+	DelayAction(function()Control.KeyDown(hotkey) end,.05)
+	DelayAction(function()Control.SetCursorPos(endPos) end,.1)
+	DelayAction(function()
+        Control.KeyUp(hotkey) 
+        orbwalker:SetMovement(true)
+        orbwalker:SetAttack(true)
+    end,.15) 
+	DelayAction(function()Control.SetCursorPos(_vectorMousePos) end,.15)
+end
+
+
+class "Ornn"
+        
+function Ornn:__init()	     
+    print("Zgjfjfl-Ornn Loaded") 
+    self:LoadMenu()
+	
+    Callback.Add("Draw", function() self:Draw() end)
+    Callback.Add("Tick", function() self:onTickEvent() end)
+    self.qSpell = {Type = GGPrediction.SPELLTYPE_LINE, Delay = 0.25, Radius = 65, Range = 750, Speed = 1800, Collision = false}
+    self.wSpell = {Type = GGPrediction.SPELLTYPE_LINE, Delay = 0, Radius = 175, Range = 500, Speed = math.huge, Collision = false}
+    self.eSpell = {Type = GGPrediction.SPELLTYPE_LINE, Delay = 0.35, Radius = 180, Range = 800, Speed = 1600, Collision = false}
+
+  end
+
+function Ornn:LoadMenu() 
+    self.Menu = MenuElement({type = MENU, id = "zgOrnn", name = "Zgjfjfl Ornn"})
+            
+    self.Menu:MenuElement({type = MENU, id = "Combo", name = "Combo"})
+        self.Menu.Combo:MenuElement({id = "Q", name = "[Q]", toggle = true, value = true})
+        self.Menu.Combo:MenuElement({id = "W", name = "[W]", toggle = true, value = true})
+        self.Menu.Combo:MenuElement({id = "E", name = "[E]", toggle = true, value = true})
+
+    self.Menu:MenuElement({type = MENU, id = "Harass", name = "Harass"})
+        self.Menu.Harass:MenuElement({id = "Q", name = "[Q]", toggle = true, value = true})
+        self.Menu.Harass:MenuElement({id = "W", name = "[W]", toggle = true, value = true})
+
+    self.Menu:MenuElement({type = MENU, id = "Clear", name = "Lane Clear"})
+        self.Menu.Clear:MenuElement({id = "W", name = "[W]", toggle = true, value = true})
+
+    self.Menu:MenuElement({type = MENU, id = "Draw", name = "Draw"})
+        self.Menu.Draw:MenuElement({id = "Q", name = "[Q] Range", toggle = true, value = false})
+        self.Menu.Draw:MenuElement({id = "E", name = "[E] Range", toggle = true, value = false})
+
+end
+
+function Ornn:onTickEvent()
+
+    if _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_COMBO] then
+        self:Combo()
+    end
+    if _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_HARASS] then
+        self:Harass()
+    end
+    if _G.SDK.Orbwalker.Modes[_G.SDK.ORBWALKER_MODE_LANECLEAR] then
+        self:LaneClear()
+    end
+   
+end
+
+function Ornn:castQE(target)
+    local pos = myHero.pos + (target.pos - myHero.pos):Normalized() * 750
+    self.isInEQ = true
+
+        castSpellHigh(self.qSpell, HK_Q, target)
+        DelayAction(function()
+            Control.CastSpell(HK_E, pos)
+            self.isInEQ = false
+        end, 1.375
+        )
+end
+
+
+
+function Ornn:Combo()
+
+    if _G.SDK.Attack:IsActive() then return end
+
+    local target = _G.SDK.TargetSelector:GetTarget(1000, _G.SDK.DAMAGE_TYPE_PHYSICAL);
+    if target then
+
+        if self.Menu.Combo.Q:Value() and isSpellReady(_Q) and myHero.pos:DistanceTo(target.pos) <= self.qSpell.Range then
+            self:castQE(target)
+        end
+
+        if self.Menu.Combo.W:Value() and isSpellReady(_W) and myHero.pos:DistanceTo(target.pos) <= self.wSpell.Range then
+            castSpellHigh(self.wSpell, HK_W, target)
+        end
+
+        local direction = (target.pos - myHero.pos):Normalized()
+        for distance = 50, 800, 50 do
+            local EPosition = myHero.pos + direction * distance
+            local castPos = target.pos:Extended(myHero.pos, -500)
+            local lineE = LineSegment(target.pos, castPos, EPosition)
+            if MapPosition:inWall(EPosition) and MapPosition:intersectsWall(lineE) and target.pos:DistanceTo(EPosition) < 300 and self.Menu.Combo.E:Value() then
+                Control.CastSpell(HK_E, EPosition)
+            end
+        end
+    end
+end	
+
+function Ornn:LaneClear()
+    local target = HealthPrediction:GetJungleTarget()
+    if not target then
+        target = HealthPrediction:GetLaneClearTarget()
+    end
+    if target then
+        if self.Menu.Clear.W:Value() and isSpellReady(_W) then
+            bestPosition, bestCount = getAOEMinion(self.wSpell.Range, self.wSpell.Radius)
+            if bestCount > 0 then 
+                Control.CastSpell(HK_W, bestPosition)
+            end
+        end
+    end
+end
+
+function Ornn:Harass()
+
+    if _G.SDK.Attack:IsActive() then return end
+
+    local target = _G.SDK.TargetSelector:GetTarget(self.qSpell.Range, _G.SDK.DAMAGE_TYPE_PHYSICAL);
+    if target then
+            
+        if self.Menu.Harass.Q:Value() and isSpellReady(_Q) and myHero.pos:DistanceTo(target.pos) <= self.qSpell.Range then
+            castSpellHigh(self.qSpell, HK_Q, target)
+        end
+        if self.Menu.Harass.W:Value() and isSpellReady(_W) and myHero.pos:DistanceTo(target.pos) <= self.qSpell.Range then
+            castSpellHigh(self.qSpell, HK_W, target)
+        end
+    end
+end
+
+function Ornn:Draw()
+   if self.Menu.Draw.Q:Value() and isSpellReady(_Q)then
+            Draw.Circle(myHero.pos, self.qSpell.Range, Draw.Color(192, 255, 255, 255))
+    end
+    if self.Menu.Draw.E:Value() and isSpellReady(_E)then
+            Draw.Circle(myHero.pos, self.eSpell.Range, Draw.Color(192, 255, 255, 255))
+    end
+end
+
+
+function onLoadEvent()
+    if table.contains(Heroes, myHero.charName) then
+		_G[myHero.charName]()
+    end
+end
+
+
+Callback.Add('Load', onLoadEvent)
